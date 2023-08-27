e80a0aa504c3abf3df06405e036c963c11dbd0c6
Noman <mdnoman70499@gmail.com>
2023-08-27 11:30:36 +0530

    Noman add

--- Datatypes.js
+++ Datatypes.js
@@ -2,6 +2,7 @@
 // alert (3+3) // we are using node.js, not browser
 console.log(3 + 3); // code readability should be high
 console.log("hitesh");
+console.log("Noman");
 
 let name = "Noman";
 let age = 20;
